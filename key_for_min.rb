bf64749e5aed597e3cdc10199f7c60f87e5e7f56
Kevin Malazarte <kevin.malazarte@gmail.com>
2019-03-20 01:32:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,10 @@ def key_for_min_value(name_hash)
     if name_hash == {}
       return nil
     else 
-      low = 
+      low = 10000
       low_key_ = ""
       name_hash.each do |key, value|
-        if value < 10000
+        if value < low
         low_key_ = key
         
 end
